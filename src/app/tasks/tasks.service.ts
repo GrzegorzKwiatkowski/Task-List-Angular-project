56c51b04b33b97e54a31622b511dcb059876377e
Grzegorz Kwiatkowski <beo.kwiatkowski@gmail.com>
2024-06-24 20:54:09 +0200

    Using localStorage for Data Storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -28,6 +28,15 @@ export class TaskService {
           dueDate: '2024-06-15',
         },
       ];
+
+constructor(){
+  const tasks = localStorage.getItem('tasks')
+
+  if (tasks) {
+this.tasks = JSON.parse(tasks);
+  }
+}
+
       getUserTasks(userId:string){
         return this.tasks.filter((task)=> task.userId === userId);
       }
@@ -40,9 +49,15 @@ export class TaskService {
             summary: taskData.summary,
             dueDate: taskData.date
           })
+          this.saveTasks();
       }
 
       removeTask(id:string) {
         this.tasks = this.tasks.filter((task)=> task.id !== id)
+        this.saveTasks();
+      }
+
+      private saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks))
       }
 }
